ea7cf2d5695fe240c2fa0726ad331a2203e3d637
Stepan Orlov <majorsteve@mail.ru>
2017-05-10 13:08:50 +0300

    Added some methods to further implement.

--- vibro-transport/VibroTransport.h
+++ vibro-transport/VibroTransport.h
@@ -29,15 +29,31 @@ class VibroTransport :
 
         virtual void rhs( V& dst, real_type time, const V& x ) const
             {
-            const real_type g = 9.8;
-       
             this->odeRhsPreObservers( time, x, this );
+            switch (m_discreteState) {
+                case F:
+                    ASSERT(false); // TODO
+                    break;
+                case S:
+                    {
+                    real_type N, R;
+                    computeReactions_S(N, R, time, x);
+                    ASSERT(false); // TODO
+                    break;
+                    }
+                case K:
+                    ASSERT(false); // TODO
+                    break;
+                default:
+                    ASSERT(false);
+                }
+
 			dst.resize(4);
 			auto v1 = x[2];
 			auto v2 = x[3];
 			auto v = sqrt(v1*v1 + v2*v2);
-			auto m_F1 = -g*sin(m_alf) + R + pow(m_ow, 2)*A*sin(m_ow*time); //*************************Где нужно высчитывать R и N?
-			auto m_F2 = -g*cos(m_alf) + N + pow(m_ow, 2)*B*cos(m_ow*time + m_Eps);
+            auto m_F1 = 0;// TODO -g*sin(m_alf) + R + pow(m_ow, 2)*A*sin(m_ow*time); //*************************Где нужно высчитывать R и N?
+            auto m_F2 = 0;// TODO -g*cos(m_alf) + N + pow(m_ow, 2)*B*cos(m_ow*time + m_Eps);
 			dst[0] = v1; //************************************************************это x с точкой?
 			dst[1] = v2; // ***********************************************************это y с точкой?
 			dst[2] = m_F1 / m_mass;
@@ -69,7 +85,7 @@ class VibroTransport :
 			}*/
             }
 
-        virtual void switchPhaseState( const int* /*transitions*/ ) {
+        virtual void switchPhaseState( const int* /*transitions*/, real_type /*time*/, V& /*x*/ ) {
             }
         virtual std::string describeZeroFunction( unsigned int /*index*/ ) const {
             return std::string();
@@ -121,6 +137,20 @@ class VibroTransport :
 		const real_type B = 4;//Амлитуда по y
 		const real_type m_Eps = 0.5;//смещение фазы
 
+        template< class T >
+        static T sqr( T x ) {
+            return x*x;
+            }
+        void computeReactions_S(real_type& N, real_type& R, real_type time, const V& x) const
+            {
+            ASSERT(m_discreteState == S);
+            ASSERT(false); // TODO
+            }
+        void computeReactions_K(real_type& N, real_type& R, real_type time, const V& x) const
+            {
+            ASSERT(m_discreteState == K);
+            ASSERT(false); // TODO
+            }
     };
 
 #endif // _VIBRO_TRANSPORT_H_
