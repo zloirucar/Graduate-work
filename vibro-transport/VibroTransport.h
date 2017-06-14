84e759383785fa9ad3fbf1c02b276e5b30a0ed87
Stepan Orlov <majorsteve@mail.ru>
2017-06-14 19:22:43 +0300

    Working on state switching, take 1.

--- vibro-transport/VibroTransport.h
+++ vibro-transport/VibroTransport.h
@@ -2,6 +2,7 @@
 #define _VIBRO_TRANSPORT_H_
 
 #include "ode_num_int/OdeRhs.h"
+#include "ode_num_int/util/m_const.h"
 
 using namespace ctm;
 using namespace math;
@@ -31,43 +32,34 @@ class VibroTransport :
             {
             this->odeRhsPreObservers( time, x, this );
 			dst.resize(4);
-			real_type N;
-			real_type R,R0;
             switch (m_discreteState) {
                 case F1:
+                case F:
 					dst[0] = x[2];
 					dst[1] = x[3];
 					dst[2] = -g*sin(m_alf) + sqr(m_ow)*A*sin(m_ow*time);
 					dst[3] = -g*cos(m_alf) + sqr(m_ow)*B*sin(m_ow*time+m_Eps);
                     break;
-				case F:
-					dst[0] = x[2];
-					dst[1] = x[3];
-					dst[2] = -g*sin(m_alf) + sqr(m_ow)*A*sin(m_ow*time);
-					dst[3] = -g*cos(m_alf) + sqr(m_ow)*B*sin(m_ow*time + m_Eps);
-					break;
-                case S:
+                case SL:
+                case SR:
                     {
-                    computeReactions_S(N, R, time, x);
-					dst[0] = x[2];
+                    auto N = normalReaction(time, x);
+                    auto R = -sgn(x[2]) * slidingFrictionForceNorm(N);
+                    dst[0] = x[2];
 					dst[1] = x[3];
 					dst[2] = -g*sin(m_alf) + R/m_mass + sqr(m_ow)*A*sin(m_ow*time);
-					dst[3] = -g*cos(m_alf) + N/m_mass + sqr(m_ow)*B*sin(m_ow*time + m_Eps);
+                    dst[3] = 0;
 				    break;
                     }
                 case K:
-					computeReactions_K(N, R,R0, time, x);
 					dst[0] = x[2];
 					dst[1] = x[3];
 					dst[2] = 0;
 					dst[3] = 0;
-
                     break;
-                    
-			}
+                }
             this->odeRhsPostObservers( time, x, dst, this );
-		};
-
+            }
 		
 
         virtual unsigned int zeroFuncCount() const
@@ -80,96 +72,94 @@ class VibroTransport :
 			real_type N, R, R0;
 			dst.resize(2);
 			switch (m_discreteState) {
-			case F:
-				dst[0] = x[1];
-				dst[1] = 1;
-				break;
-			case F1:
-				dst[0] = x[3]-min_speed;
-				dst[1] = x[1];
-				break;
-			case S:
-				computeReactions_S(N, R, time, x);
-				dst[0] = N;
-				dst[1] = x[2];
-				break;
-			case K:
-				computeReactions_K(N, R, R0, time, x);
-				dst[0] = N;
-				dst[1] = R0-R;
-				break;
-			};
-
-			};
+                case F:
+                    dst[0] = x[1];
+                    dst[1] = 1;
+                    break;
+                case F1:
+                    dst[0] = min_speed - x[3];
+                    dst[1] = x[1];
+                    break;
+                case SL:
+                    dst[0] = normalReaction(time, x);
+                    dst[1] = -x[2];
+                    break;
+                case SR:
+                    dst[0] = normalReaction(time, x);
+                    dst[1] = x[2];
+                    break;
+                case K: {
+                    auto N = normalReaction(time, x);
+                    dst[0] = N;
+                    dst[1] = slidingFrictionForceNorm(N) - fabs(stickingFrictionForce(time));
+                    break;
+                    }
+                }
+            }
 
 
 			virtual std::vector<unsigned int> zeroFuncFlags() const {
-				return std::vector<unsigned int>(2, OdeRhs<VD>::Discontinuous | OdeRhs<VD>::BothDirections);
-			};
+                return std::vector<unsigned int>(2, OdeRhs<VD>::PlusMinus | OdeRhs<VD>::RecomputeAfterSwitch);
+            }
 
 
-			virtual void switchPhaseState(int* transitions, real_type time, V& x) {
-				real_type N, R, R0;
-				computeReactions_K(N, R, R0, time, x);
+            virtual void switchPhaseState(const int* transitions, real_type time, V& x) {
 				switch (m_discreteState) {
 				case F1:
-					if (x[1] > 0 || N < 0 ) {
-						transitions[0] = 1;
-						m_discreteState = F;
-					} else 
-					m_discreteState = S;
+                    if (transitions[0] != 0)
+                        m_discreteState = F;
+                    else {
+                        ASSERT(transitions[1] != 0);
+                        m_discreteState = x[0] >=0? SR: SL;
+                    }
 					break;
-				case F:
+                case F: {
 					ASSERT(transitions[0] != 0);
+                    auto fallingDown = false;
 					if (x[3] < 0) {
 						x_method(x); // удар по касательной
 						x[3] *= -y_method(); //  нормальный удар
 						x[1] = 0;
-						transitions[0] = 1;
-					}
-					else {
-						transitions[0] = 1;
-					};
-					if (fabs(x[3]) < min_speed) {
+                        fallingDown = true;
+                    }
+                    if (!fallingDown   ||   fabs(x[3]) < min_speed) {
 						x[1] = 0;
 						x[3] = 0;
-						m_discreteState = S;
+                        m_discreteState = x[2] >= 0? SR: SL;
 					}
 					break;
-				case S:
-					if (fabs(R0) < fabs(R)) {
-						x[2] = 0;
-						x[1] = 0;
-						m_discreteState = K;
-					};
-					if (N < 0) {
-						x[1] = 0;
-						m_discreteState = F1;
-					};
+                    }
+                case SL:
+                case SR:
+                    if (transitions[0] != 0)
+                        m_discreteState = F1;
+                    else {
+                        ASSERT(transitions[1] != 0);
+                        auto N = normalReaction(time, x);
+                        if (fabs(stickingFrictionForce(time)) < slidingFrictionForceNorm(N))
+                            m_discreteState = K;
+                        else
+                            m_discreteState = m_discreteState == SL? SR: SL;
+                    }
 					break;
 				case K:
-					if (fabs(R) < fabs(R0) ) {
-						x[3] = 0;
-						m_discreteState = S;
-					};
-					if (N < 0) {
-						x[1] = 0;
-						x[3] = 0;
-						m_discreteState = F1;
-					}
-
-					break;
+                    if (transitions[0] != 0)
+                        m_discreteState = F1;
+                    else {
+                        ASSERT(transitions[1] != 0);
+                        m_discreteState = stickingFrictionForce(time) >= 0? SL: SR;
+                    }
+                    break;
 				default:
 					ASSERT(false); // Unreachable
 				}
-
 			}
 
 			virtual std::string describeZeroFunction(unsigned int /*index*/) const {
 				return std::string();
 			}
 
-			enum DiscreteState { F, S, K, F1 };
+            enum DiscreteState { F, SL, SR, K, F1 };
 			DiscreteState discreteState() const {
 				return m_discreteState;
 			}
@@ -180,26 +170,25 @@ class VibroTransport :
 
 			void computeDiscreteState(double time, const V& state)
 			{
-				real_type N, R, R0;
-				computeReactions_K(N, R, R0, time, state);
-				if (state[1] == 0 && state[2] == 0) {
-					/* (N < 0) {
-						m_discreteState = F;
-					} else*/ m_discreteState = K;
-				} 
-
-				else if (state[1] == 0 && state[2] != 0) {
-					/*if (N < 0) {
-						m_discreteState = F;
-					} else*/ m_discreteState = S; 
-				}
-		
-				else m_discreteState = F;
-
-				}
-			
-
-
+                if (state[1] > 0)
+                    m_discreteState = F;
+                else if (state[1] < 0)
+                    throw cxx::exception("Invalid state: x[1] must be nonnegative");
+                else {
+                    auto N = normalReaction(time, state);
+                    if (N <= 0)
+                        m_discreteState = F;
+                    else {
+                        if (state[2] == 0) {
+                            auto R = slidingFrictionForceNorm(N);
+                            auto R0 = stickingFrictionForce(time);
+                            m_discreteState = fabs(R0) < R ?   K :   R0 >= 0? SR: SL;
+                            }
+                        else
+                            m_discreteState = state[2] >= 0? SR: SL;
+                        }
+                    }
+            }
 
 	private:
 
@@ -207,67 +196,58 @@ class VibroTransport :
 
 		//Объявление переменных
 		const real_type g = 9.8;
-		const real_type m_alf = 3.14 / 6; // угол наклона поверхности
-		const real_type m_ow = 50; // частота колебаний
+        const real_type m_alf = M_PI / 6; // угол наклона поверхности
+        const real_type m_ow = 30; // частота колебаний
 		const real_type m_f = 0.8; // трение тела о лоток
 		const real_type m_mass = 1; //масса т.т.
-		const real_type A = 0.01;//Амплитуда по х
-		const real_type B = 0.05;//Амлитуда по y
-		const real_type m_Eps = 0.5;//смещение фазы
-		const real_type min_speed = 0.05; // минимальная скорость для отскока
+        const real_type A = 0.01;//Амплитуда по х
+        const real_type B = 0.01;//Амлитуда по y
+        const real_type m_Eps = 0.5*M_PI;//смещение фазы
+        const real_type min_speed = 1e-4; // минимальная скорость для отскока
 		
 		
 		template< class T >
 		static T sqr(T x) {
 			return x*x;
-		};
+        }
+
+        template< class T >
+        static T sgn(T x) {
+            return x > 0? 1: x<0? -1: 0;
+        }
 
 
-		void computeReactions_S(real_type& N, real_type& R, real_type time, const V& x) const
-		{
-			N = m_mass*g*cos(m_alf) - m_mass*sqr(m_ow)*B*sin(m_ow*time + m_Eps);
-			if (x[2] > 0) {
-				R = -m_f*N;
-			}
-			else {
-				R = m_f*N;
-			}
-		}
+        real_type normalReaction(real_type time, const V& x) const
+        {
+            return m_mass*g*cos(m_alf) - m_mass*sqr(m_ow)*B*sin(m_ow*time + m_Eps);
+        }
 
-        void computeReactions_K(real_type& N, real_type& R,real_type& R0, real_type time, const V& x) const
-            {
-			N = m_mass*g*cos(m_alf) - m_mass*sqr(m_ow)*B*sin(m_ow*time + m_Eps);
-			
-			if (x[2] > 0) {
-				R = -m_f*N;
-			}
-			else {
-				R = m_f*N;
-			};
+        real_type slidingFrictionForceNorm(real_type normalReaction) const {
+            return m_f * fabs(normalReaction);
+        }
 
-			R0 = m_mass*g*sin(m_alf) - m_mass*sqr(m_ow)*A*sin(m_ow*time);
-		};
-   
+        real_type stickingFrictionForce(real_type time) const {
+            return m_mass*g*sin(m_alf) - m_mass*sqr(m_ow)*A*sin(m_ow*time);
+        }
 
-	real_type y_method() {
-		real_type R=0.8;
-		// Здесь будет метод вычисления коэфициента восстановления
-		// TODO
-		return R;
+        real_type y_method() const {
+            real_type R=0.8;
+            // Здесь будет метод вычисления коэфициента восстановления
+            // TODO
+            return R;
 
-	};
+        }
 
-	void x_method(V& x)  {
-		
-		real_type lam;
-		lam = m_f; //Иногда принмают как коэф трения скольжения
-		if (fabs(x[2]) < fabs(m_f*(y_method()*x[3] - x[3]) / lam)) {
-			x[2] *= (1 - lam);
-		}
-		else {
-			x[2] = x[2] - m_f*(y_method()*x[3] - x[3])*Sgn(x[2]);
-		};
-	}
+        void x_method(V& x) const {
+            real_type lam;
+            lam = m_f; //Иногда принмают как коэф трения скольжения
+            if (fabs(x[2]) < fabs(m_f*(y_method()*x[3] - x[3]) / lam)) {
+                x[2] *= (1 - lam);
+            }
+            else {
+                x[2] = x[2] - m_f*(y_method()*x[3] - x[3])*Sgn(x[2]);
+            }
+        }
 
 	};
 
--- vibro-transport/main.cpp
+++ vibro-transport/main.cpp
@@ -29,6 +29,7 @@ int main()
         cfg.setValue("solver", "dopri_56");
         cfg.setValue("solver.stepsizectl.tolerance", 1e-5);
         cfg.setValue("time", 10);
+        cfg.setValue("output_timing", 0.1);
         V x0( 4 );
         x0[0] = 0;
 		x0[1] = 0;
@@ -39,6 +40,18 @@ int main()
         ASSERT(vt);
         vt->setDiscreteState(VibroTransport<VD>::F);
         vt->computeDiscreteState( 0, x0 );
+        auto stateName = [&vt]() -> string {
+            const char *stateNames[] = { "F", "SL", "SR", "K", "F1" };
+            return stateNames[vt->discreteState()];
+        };
+        cout << "INITIAL DISCRETE STATE: " << stateName() << endl;
+//        sc.solver()->odeSolverPostObservers.add([&](const OdeSolverPostObserverArg<VD>& arg) {
+//            if (!(arg.stepAccepted() && arg.stepTruncated()))
+//                return;
+//            static unsigned int counter = 0;
+//            ++counter;
+//            cout << counter << ", t=" << arg.solver()->initialTime() << ", TRANSITION: " << arg.izfTrunc() << ":" << arg.transitionType() << " -> " << stateName() << endl;
+//        });
         solveOde( &cfg, &sc );
         return 0;
     }
